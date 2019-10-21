bf356d410cff0609ed2145ba4e17c1de36faa7b9
Sameen Shakeel <sameenshakeel1997@gmail.com>
2019-10-21 15:20:22 +0500

    first commit

---
+++ abc.py
@@ -0,0 +1,11 @@
+def greeting():
+    print("Hello World!")
+    
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+
+print("Hello")
---
+++ mymodule.py
@@ -0,0 +1,8 @@
+def greeting():
+    print("Hello World!")
+    
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
